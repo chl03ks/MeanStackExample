9842c8ba4a45bac4be3a3471b3cd51bbc81d11e5
Daniel <dan@hectic.io>
2015-12-20 17:31:57 -0600

    Angular Barebones app

---
+++ public/app/app.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+    angular.module('app', ['ngResource', 'ngRoute'])
+        .config(['$routeProvider',
+            function($routeProvider) {
+                $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/main',
+                    controller: 'mainCtrl'
+                });
+            }
+        ])
+        .controller('mainCtrl', function($scope) {
+            $scope.myVar = "WTF";
+        });
+
+}());
